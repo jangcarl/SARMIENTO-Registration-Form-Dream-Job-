c15ef131f3f86157dbc04c102c35a1f352686c11
jangcarl <70869323+jangcarl@users.noreply.github.com>
2024-10-15 20:53:24 +0800

    Update dbConfig.php

--- dbConfig.php
+++ dbConfig.php
@@ -4,7 +4,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$dbname = "oct9_talaboc";
+$dbname = "oct9_sarmiento";
 $dsn = "mysql:host={$host};dbname={$dbname}";
 $pdo = new PDO($dsn, $user, $password);
 

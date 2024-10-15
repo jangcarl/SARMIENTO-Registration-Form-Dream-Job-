e2e86cbf40ee5dbac5b002dd394e93d196f21354
jangcarl <70869323+jangcarl@users.noreply.github.com>
2024-10-15 21:57:37 +0800

    Delete testGetVariable.php

--- testGetVariable.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if (isset($_GET['developerName'])) {
-    echo "<h2>Developer Name: " . $_GET['developerName']. "</h2>";
-}
-if (isset($_GET['experienceLevel'])) {
-    echo "<h2>Experience Level: " . $_GET['experienceLevel'] . "</h2>";
-}
-?>
